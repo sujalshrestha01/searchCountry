416832f7cbbd3febbc4f0caf155f8a9ce17b9d52
Sujal Shrestha <sujal8696@gmail.com>
2024-08-24 17:58:54 +0545

    implement light and dark mode

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import './style.css'
+import { jsxs } from "react/jsx-runtime";
+import { json } from "react-router-dom";
 
 
 export default function Header() {
+  const [isDark,setIsDark]=useState(JSON.parse(localStorage.getItem('isDark')))
+  
+  if(isDark){
+    document.body.classList.add('dark')
+  }
+  else{
+    document.body.classList.remove('dark')
+  }
+  
+  
+  function darkMode(){
+    setIsDark(!isDark)
+    
+  }
   return (
     
     <div className="header max-width  ">
       <h2>Where in the world?</h2>
-      <div  className="theme-changer ">
-      <i className="fa-regular fa-moon"></i>
-      <p>Dark Mode</p>
+      <div  className="theme-changer " onClick={darkMode}>
+      <i className={`fa-regular fa-${isDark?"sun":"moon"}`} ></i>
+      <p>{isDark?"Light mode":"Dark mode"}</p>
       </div>
     </div>
    
