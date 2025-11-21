6f9c7ef9a290e755010b53ecd3a02e575718ce4b
Sujal Shrestha <sujal8696@gmail.com>
2025-11-21 18:24:08 +0545

    basename for react router

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 import { createRoot } from "react-dom/client";
-import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
 import App from "./App";
 import CountryDetail from "./Components/CountryDetail";
 import Home from "./Components/Home";
 
-const router = createBrowserRouter([
+// Add the basename (your repo name)
+const basename = "/searchCountry"; 
+
+const router = createBrowserRouter(
+  [
     {
       path: "/",
-      element: <App/>,
-      children:[
-        {
-            path:"/",
-            element:<Home/>
-        },
-        {
-            path:"/:country",
-            element:<CountryDetail/>
-        }
-      ]
+      element: <App />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/:country", element: <CountryDetail /> },
+      ],
     },
-   
-  ]);
-  
+  ],
+  { basename } 
+);
 
-let root=createRoot(document.querySelector('#root'))
-root.render(<RouterProvider router={router} />)
+const root = createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
